2e41ce19b1825a4043b811c24cfee959930ee0d5
Ric0 <ariazjean@gmail.com>
2025-03-04 12:43:56 -0400

    extract_total_codes working

--- src/lib/web_code_extractor/mod.rs
+++ src/lib/web_code_extractor/mod.rs
@@ -1,52 +1,56 @@
 use reqwest;
-/// web_code_extractor module extracts *coupon codes* from a web page
-/// ## returns coupon codes from the page
-pub struct web_code_extractor {
-    pub url: String,
+use scraper::Html;
+/// # web_code_extractor
+/// Extracts *coupon codes* from a web page and
+/// returns information of the coupon codes from the page to analityze
+pub struct WebCodeExtractor {
+    /// **URL of the page**
+    url: String,
+    /// **The coupon codes that you have**
     pub my_coupon_codes: Vec<String>,
+    /// **The coupon code extracted from the page**
     pub coupon_code_from_the_page: String,
+    /// **The HTML document**
+    document: Html,
+    /// item code tag writed as a normal querySelector
+    item_code_tag:String,
 }
 
-pub trait web_code_extractor_trait {
+impl WebCodeExtractor {
     // Extract the coupon code from the page
-     fn new(url: String, my_coupon_codes: Vec<String>) -> web_code_extractor;
-
-     fn extract_total_codes(&mut self) -> Vec<String>;
-
-     fn get_code_Info(&self) -> &str;
-
-}
-
-impl web_code_extractor_trait for web_code_extractor {
-    // Extract the coupon code from the page
-     fn new(url: String, my_coupon_codes: Vec<String>) -> web_code_extractor {
-        let response = reqwest::blocking::get("https://www.scrapingcourse.com/ecommerce/");
+    pub fn new(url: String, item_code_tag:String, my_coupon_codes: Vec<String>) -> WebCodeExtractor {
+        let response = reqwest::blocking::get(&url);
         let html_content = response.unwrap().text().unwrap();
         let document = scraper::Html::parse_document(&html_content);
-        
-        // Extract the Coupon list Elements as a selector
-        let html_product_selector = scraper::Selector::parse("li.product").unwrap();
-        let html_products = document.select(&html_product_selector);
 
-
-
-        web_code_extractor {
+        WebCodeExtractor {
             url,
             my_coupon_codes,
             coupon_code_from_the_page: String::new(),
+            document,
+            item_code_tag
         }
     }
 
-     fn extract_total_codes(&mut self) -> Vec<String> {
-        
+    pub fn extract_total_codes(&mut self) -> Vec<String> {
 
-        todo!();
+        // Extract the Coupon list Elements as a selector
+        let html_product_selector = scraper::Selector::parse(&self.item_code_tag).unwrap();
+        let html_products = self.document.select(&html_product_selector);
+        for coupon_tag in html_products {
+
+            // Extract the Coupon code from the element
+            let coupon_code = coupon_tag.attr("value").unwrap();
+            self.my_coupon_codes.push(coupon_code.to_owned());
+        }
+
+        return self.my_coupon_codes.to_owned();
         // Implementación para extraer el código de la página web
         // self.coupon_code_from_the_page = format!("Extracted code from {}", self.url);
     }
 
      fn get_code_Info(&self) -> &str {
-        todo!()
+        todo!("Display Coupons Info of the store")
     }
 
-}
+}   
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 mod lib;
 
-use lib::web_code_extractor::{web_code_extractor,web_code_extractor_trait};
+use lib::Web_Code_Extractor::WebCodeExtractor;
 
 fn main() {
     let my_coupon_codes: Vec<String> = vec!["CODE1".to_string(), "CODE2".to_string()];
-    let web_code_extracted:web_code_extractor = web_code_extractor::new(
-        "https://dealspotr.com/promo-codes/vegega.com/74014427".to_owned()
-        , my_coupon_codes);    
+    let mut web_code_extracted:WebCodeExtractor = WebCodeExtractor::new(
+        "https://dealspotr.com/promo-codes/vegega.com/74014427".to_owned(),
+        "input.dnone".to_owned(),
+        my_coupon_codes);    
+    let extracted_codes = web_code_extracted.extract_total_codes();
+
     
-    print!("{}",web_code_extracted.get_code_Info());
-    
-    
-    
+        
+    print!("{:?}",extracted_codes)
 
 
 
