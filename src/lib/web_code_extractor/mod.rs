9c848067af59a81b43e8fe23176f08495c72bfdf
Ric0 <ariazjean@gmail.com>
2025-03-05 17:17:03 -0400

    adding coupon code methods

--- src/lib/web_code_extractor/mod.rs
+++ src/lib/web_code_extractor/mod.rs
@@ -9,48 +9,83 @@ pub struct WebCodeExtractor {
     /// **The coupon codes that you have**
     pub my_coupon_codes: Vec<String>,
     /// **The coupon code extracted from the page**
-    pub coupon_code_from_the_page: String,
+    pub coupon_code_from_the_page: Vec<CouponCode>,
     /// **The HTML document**
     document: Html,
     /// item code tag writed as a normal querySelector
     item_code_tag:String,
 }
 
-impl WebCodeExtractor {
+pub trait WebCodeExtractorTrait {
     // Extract the coupon code from the page
-    pub fn new(url: String, item_code_tag:String, my_coupon_codes: Vec<String>) -> WebCodeExtractor {
-        let response = reqwest::blocking::get(&url);
-        let html_content = response.unwrap().text().unwrap();
-        let document = scraper::Html::parse_document(&html_content);
+     fn new(url: String, item_code_tag:String, my_coupon_codes: Vec<String>) -> WebCodeExtractor;
+
+     fn extract_codes(&mut self) -> &Vec<CouponCode>;
+
+     fn get_code_Info(&self,code_name:String) ->CouponCode;
+
+}
+
+impl WebCodeExtractorTrait for WebCodeExtractor {
+    // Extract the coupon code from the page
+     fn new(url: String, item_code_tag:String, my_coupon_codes: Vec<String>) -> WebCodeExtractor {
+        let response: Result<reqwest::blocking::Response, reqwest::Error> = reqwest::blocking::get(&url);
+        let html_content: String = response.unwrap().text().unwrap();
+        let document: Html = scraper::Html::parse_document(&html_content);
 
         WebCodeExtractor {
             url,
             my_coupon_codes,
-            coupon_code_from_the_page: String::new(),
+            coupon_code_from_the_page: Vec::new(),
             document,
             item_code_tag
         }
     }
 
-    pub fn extract_total_codes(&mut self) -> Vec<String> {
+    fn extract_codes(&mut self) -> &Vec<CouponCode> {
 
         // Extract the Coupon list Elements as a selector
-        let html_product_selector = scraper::Selector::parse(&self.item_code_tag).unwrap();
-        let html_products = self.document.select(&html_product_selector);
+        let html_product_selector: scraper::Selector = scraper::Selector::parse(&self.item_code_tag).unwrap();
+        let html_products: scraper::html::Select<'_, '_> = self.document.select(&html_product_selector);
         for coupon_tag in html_products {
 
             // Extract the Coupon code from the element
-            let coupon_code = coupon_tag.attr("value").unwrap();
-            self.my_coupon_codes.push(coupon_code.to_owned());
+            self.coupon_code_from_the_page.push(
+                CouponCode{
+                    code_name:coupon_tag.select(&scraper::Selector::parse("input.dnone").unwrap())
+                             .next().unwrap().value().attr("value").unwrap().to_owned(),
+                    code_uses:coupon_tag.select(&scraper::Selector::parse("span.vam").unwrap())
+                             .next().unwrap().text().collect::<Vec<_>>().join(" "),
+                    description: "This is a coupon code".to_owned(),
+                }
+            );
         }
-
-        return self.my_coupon_codes.to_owned();
+        return &self.coupon_code_from_the_page;
+        // return self.coupon_code_from_the_page.to_owned();
         // Implementación para extraer el código de la página web
         // self.coupon_code_from_the_page = format!("Extracted code from {}", self.url);
     }
 
-     fn get_code_Info(&self) -> &str {
-        todo!("Display Coupons Info of the store")
-    }
-
+    fn get_code_Info(&self, code_name: String) -> CouponCode {
+            self.coupon_code_from_the_page.iter().find(|coupon| coupon.code_name == code_name)
+                .cloned()
+                .unwrap_or_else(|| CouponCode {
+                    code_name: "Not Found".to_string(),
+                    code_uses: "0".to_string(),
+                    description: "No description available".to_string(),
+                })
+        }
 }   
+
+#[derive(Debug, Clone)]
+pub struct CouponCode{
+
+    code_name: String,
+    code_uses: String,
+    description: String,
+
+}
+
+impl CouponCode{
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,16 @@
 mod lib;
-
-use lib::Web_Code_Extractor::WebCodeExtractor;
-
+use lib::Web_Code_Extractor::{WebCodeExtractor, WebCodeExtractorTrait};
 fn main() {
     let my_coupon_codes: Vec<String> = vec!["CODE1".to_string(), "CODE2".to_string()];
     let mut web_code_extracted:WebCodeExtractor = WebCodeExtractor::new(
-        "https://dealspotr.com/promo-codes/vegega.com/74014427".to_owned(),
-        "input.dnone".to_owned(),
-        my_coupon_codes);    
-    let extracted_codes = web_code_extracted.extract_total_codes();
+        "https://dealspotr.com/promo-codes/vegega.com/".to_owned(),
+        "div.copy-code".to_owned(),
+        my_coupon_codes);
+        web_code_extracted.extract_codes();    
 
-    
-        
-    print!("{:?}",extracted_codes)
-
-
-
+    // print!("{:?}",extracted_codes)
     // let response= getPage();
     // let html_content = response.unwrap();
     // print!("{}",html_content) 
-    
-}
 
+}
