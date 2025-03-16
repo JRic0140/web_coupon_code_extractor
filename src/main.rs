b82cd02532c9c2eea44dc13455bdf7c82bdc2c54
Ric0 <ariazjean@gmail.com>
2025-03-16 08:48:51 -0400

    making front with ui template

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use code_scraper_app::{CodeScraperAppTrait,CodeScraperApp};
 fn main() {
     let app: CodeScraperApp = CodeScraperApp::new();
     
+    
     // let my_coupon_codes: Vec<String> = vec!["CODE1".to_string(), "CODE2".to_string()];
     // let mut web_code_extracted:WebCodeExtractor = WebCodeExtractor::new(
     //     "https://dealspotr.com/promo-codes/vegega.com/".to_owned(),
